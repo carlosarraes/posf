cfd62bf5d510c4f7a390ae44c2520de374b2a9d5
carraes <carraeshb@gmail.com>
2023-09-03 01:26:25 -0300

    feat: create animal service, with crud funcs

---
+++ AnimalService.java
@@ -0,0 +1,62 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Scanner;
+
+public class AnimalService implements IAnimal {
+  private List<Animal> animais = new ArrayList<Animal>();
+  private Map<String, Animal> animalDB = new HashMap<>();
+
+  @Override
+  public void addAnimal(Animal animal) {
+    if (animalDB.containsKey(animal.getName()))
+      throw new DuplicateAnimalException("Animal já existe");
+
+    animais.add(animal);
+    animalDB.put(animal.getName(), animal);
+  }
+
+  @Override
+  public void removeAnimal(Animal animal) {
+    animais.remove(animal);
+    animalDB.remove(animal.getName());
+  }
+
+  @Override
+  public List<Animal> listAnimals() {
+    return animais;
+  }
+
+  @Override
+  public void updateAnimal(Animal animal) {
+    int index = animais.indexOf(animal);
+    animais.set(index, animal);
+    animalDB.put(animal.getName(), animal);
+  }
+
+  public void loadAnimalsFromFile(String filePath) throws FileNotFoundException {
+    Scanner scanner = new Scanner(new File(filePath));
+    while (scanner.hasNextLine()) {
+      String line = scanner.nextLine();
+      String[] parts = line.split(",");
+      String name = parts[0];
+      int age = Integer.parseInt(parts[1]);
+      boolean isDomesticated = Boolean.parseBoolean(parts[2]);
+      addAnimal(new Dog(name, age, isDomesticated));
+    }
+    scanner.close();
+  }
+
+  public void saveAnimalsToFile(String filePath) throws IOException {
+    FileWriter writer = new FileWriter(new File(filePath));
+    for (Animal animal : animalDB.values()) {
+      writer.write(animal.toString() + "\n");
+    }
+    writer.close();
+  }
+}
